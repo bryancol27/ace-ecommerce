f12d5a54639a318401ee4d767518a68f2fe0d667
Diego-Figue <diegofigue0212@gmail.com>
2023-06-13 17:09:11 -0500

    ADD: route Register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,16 +1,47 @@
+'use client';
+
 import Image from 'next/image';
 // import styles from './page.module.css';
-
-//  Import components
-import { TitleSearch } from '@/components/TitleSearch';
+import { MainStyled } from './styles';
+//import icons
+import { Mail, Lock, Anchor } from 'feather-icons-react/build/IconComponents';
 
 export default function Register() {
     return (
-        <main>
-            <TitleSearch
-                placerHolder="Busca lo que deseas"
-                title="¿Qué vas a querer comprar hoy?"
-            />
-        </main>
+        <MainStyled>
+            <h1>Register</h1>
+
+            <form>
+                <div>
+                    <label for="">
+                        <Mail />
+                        Register
+                    </label>
+                    <input type="email" />
+                </div>
+                <div>
+                    <label for="">
+                        <Lock />
+                        Contraseña
+                    </label>
+                    <input type="password" />
+                </div>
+                <div>
+                    <label for="">
+                        <Lock />
+                        Repetir contraseña
+                    </label>
+                    <input type="password" />
+                </div>
+                <div>
+                    <label for="">
+                        <Anchor />
+                        Nombre
+                    </label>
+                    <input type="text" />
+                </div>
+            </form>
+            <button type="button">Registrate</button>
+        </MainStyled>
     );
 }
---
+++ src/app/register/styles.js
@@ -0,0 +1,70 @@
+import styled from '@emotion/styled';
+
+export const MainStyled = styled.main`
+    width: 100%;
+    height: 90vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 2rem;
+
+    h1 {
+        font-weight: 500;
+        font-size: 35px;
+        line-height: 51px;
+        text-align: center;
+        color: #000000;
+    }
+
+    form {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        flex-direction: column;
+        width: 494px;
+        gap: 1.5rem;
+
+        div {
+            width: 100%;
+            margin-bottom: 20px;
+
+            label {
+                display: flex;
+                gap: .5rem;
+                font-weight: 500;
+                font-size: 20px;
+                line-height: 23px;
+                letter-spacing: 0.11em;
+
+                color: #000000;
+            }
+
+            input {
+                width: 100%;
+                height: 68px !important;
+
+                border: 1px solid rgba(0, 0, 0, 0.3);
+                border-radius: 20px;
+                padding: 0px 20px;
+
+                font-size: 28px;
+            }
+        }
+    }
+
+    button {
+        width: 244px;
+        height: 50px;
+        border: 2px solid #a4161a;
+        border-radius: 20px;
+        background: transparent;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 23px;
+        text-align: center;
+        letter-spacing: 0.11em;
+        color: #a4161a;
+        cursor: pointer;
+    }
+`;
